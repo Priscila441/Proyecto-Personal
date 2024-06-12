fe517da225d334c647cc264e8a8f6f797044827d
PriscilaCordoba <pricordoba441@gmail.com>
2024-06-12 10:18:24 -0300

    carpeta components, header, main y footer

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return (
+        <>
+            <div>Aca va una imagen</div>
+            <div>
+                <ul>
+                    <li>SHOW</li>
+                    <li>ABOUT</li>
+                    <li>OUR IMPACT</li>
+                </ul>
+            </div>
+            <div>
+                <div>img</div>
+                <div>img</div>
+            </div>
+        </>
+    )
+}
+
+export default Header
